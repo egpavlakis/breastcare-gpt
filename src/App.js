b2be778acbb5bdd99dad67ca3f80b3fd8d13275e
egpavlakis <egpavlakis@gmail.com>
2025-07-05 23:55:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<h1>Hello from BreastCare GPT!</h1>
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PatientDataEntryForm from './PatientDataEntryForm';
