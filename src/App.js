739db37e1410d61e5981855f4efe16f222c2870d
egpavlakis <egpavlakis@gmail.com>
2025-07-06 07:32:17 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PatientDataEntryForm from './PatientDataEntryForm';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>BreastCare GPT</h1>
+      <PatientDataEntryForm />
+    </div>
+  );
+}
+
+export default App;
